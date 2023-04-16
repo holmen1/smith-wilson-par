27bcf437bfb797cd07a26c001776fbb42097d0f2
holmen1 <holmen1@gmail.com>
2023-04-16 14:31:56 +0200

    Add alpha

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import numpy as np
 from smithwilson import RiskFreeRates
 
-rates = np.array([0.032, 0.0308, 0.0288, 0.0271])
+rates = np.array([0.03405, 0.03164, 0.02914, 0.02753])
 maturities = np.array([2, 3, 5, 10])
-projection = np.arange(1, 121)
+projection = np.arange(1, 151)
+#projection = np.arange(1, 12)
 ufr = 0.0345
-alpha = 0.414598
+alpha0 = 0.397593
+convergence_maturity = 20
+tol = 1e-4
 
-RFR = RiskFreeRates(rates, maturities, projection, ufr, alpha)
-r = RFR.rates()
-print(r[:19])
+RFR = RiskFreeRates(rates, maturities, projection, ufr, convergence_maturity, tol)
+alpha, r = RFR.result
+print(r[maturities-1])
+print(f'alpha = {alpha}')
+print(f'f_20 = {(1 + r[20])**21/(1 + r[19])**20 - 1}')
--- smithwilson.py
+++ smithwilson.py
@@ -10,38 +10,63 @@ import numpy as np
 
 
 class RiskFreeRates(object):
-    def __init__(self, swap_rates, swap_maturities, maturities, ufr, alpha):
-        omega = np.log(1 + ufr)
-        self.maturities = maturities
 
-        p = np.ones(swap_maturities.size)
-        C = self.cashflows(swap_rates, swap_maturities, maturities)
-        d = np.exp(-omega * maturities)
+    def __init__(self, swap_rates, swap_maturities, maturities, ufr, T, tol, alpha0=0.05):
+        # Variables as in EIOPA's Technical documentation
+        omega = np.log(1 + ufr)
+        u = maturities  # projection
+        v = swap_maturities
+        p = np.ones(v.size)
+        C = cashflows(swap_rates, v, u)
+        d = np.exp(-omega * u)
         q = C.T @ d
         Q = np.diag(d) @ C
-        H = self.heart(maturities, maturities, alpha)
+        alpha = find_alpha(alpha0, T, u, Q, p, q, tol) if alpha0 == 0.05 else alpha0
+        H = heart(u, u, alpha)
         b = np.linalg.solve(Q.T @ H @ Q, p - q)
 
-        self.price = d + np.diag(d) @ H @ Q @ b
+        price = d + np.diag(d) @ H @ Q @ b
+        rates = np.power(1 / price, 1 / u) - 1
+        self.result = (alpha, rates)
 
-    def rates(self):
-        return np.power(1 / self.price, 1 / self.maturities) - 1
 
-    def heart(self, u, v, alpha):
-        u_mat = np.tile(u, [v.size, 1]).transpose()
-        v_mat = np.tile(v, [u.size, 1])
-        return 0.5 * (alpha * (u_mat + v_mat) + np.exp(-alpha * (u_mat + v_mat)) - alpha * np.absolute(
-            u_mat - v_mat) - np.exp(-alpha * np.absolute(u_mat - v_mat)))
+def heart(u, v, alpha):
+    u_mat = np.tile(u, [v.size, 1]).T
+    v_mat = np.tile(v, [u.size, 1])
+    return 0.5 * (alpha * (u_mat + v_mat) + np.exp(-alpha * (u_mat + v_mat)) - alpha * np.absolute(
+        u_mat - v_mat) - np.exp(-alpha * np.absolute(u_mat - v_mat)))
 
-    def cashflows(self, rates, maturities, durations):
-        CT = np.zeros((maturities.size, durations.size))
-        for i in np.arange(maturities.size):
-            maturity_index = np.where(durations == maturities[i])[0]
-            for j in np.arange(durations.size):
-                if j < maturity_index:
-                    CT[i, j] = rates[i]
-                elif j == maturity_index:
-                    CT[i, j] = 1 + rates[i]
-                else:
-                    CT[i, j] = 0
-        return CT.T  # = C
+
+def cashflows(rates, maturities, durations):
+    CT = np.zeros((maturities.size, durations.size))
+    for i in np.arange(maturities.size):
+        maturity_index = np.where(durations == maturities[i])[0]
+        for j in np.arange(durations.size):
+            if j < maturity_index:
+                CT[i, j] = rates[i]
+            elif j == maturity_index:
+                CT[i, j] = 1 + rates[i]
+            else:
+                CT[i, j] = 0
+    return CT.T  # = C
+
+
+def find_alpha(alpha, t, u, Q, p, q, tol):
+    error = 1
+    step = 1e-4
+    while error > tol:
+        H = heart(u, u, alpha)
+        b = np.linalg.solve(Q.T @ H @ Q, p - q)
+        error = gap(t, alpha, u, Q, b)
+        alpha = alpha + step
+    return alpha
+
+
+def gap(t, alpha, u, Q, b):
+    kappa = quasi(alpha, u, Q, b)
+    return alpha / np.abs(1 - kappa * np.exp(alpha * t))
+
+
+# constant to determine the convergence of the alpha parameter
+def quasi(alpha, u, Q, b):
+    return (1 + alpha * u @ Q @ b) / (np.sinh(alpha * u) @ Q @ b)
